fc1f5a48b0a2b7fa823e0c03bc808a02b6192079
Pa Lee <sheeyeechang@gmail.com>
2019-06-18 10:49:57 -0500

    Added function that returns all words that contain the substring th from a list to the LINQQuestions class. Will test in Program class.

--- LINQ/LINQQuestions.cs
+++ LINQ/LINQQuestions.cs
@@ -8,5 +8,27 @@ namespace LINQ
 {
     class LINQQuestions
     {
+        //member varibale (HAS A)
+        //constructor
+
+        //method (CAN DO)
+        //1. Using LINQ, write a function that returns all words that contain the substring “th” from a list.
+        //List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", “mathematics” };
+
+        //Contains returns true or false. It indicates whether the IEnumerable collection has an element matching the argument.It acts upon any IEnumerable collection when the System.Linq namespace is included.
+       
+
+               public List<string> ReturnSubstringFromList(List<string> list)
+        {
+            List<string> thList = list.Where(t => t.Contains("th")).ToList();
+
+            return thList;
+        }
+
+
+
+        
+        
+
     }
 }
